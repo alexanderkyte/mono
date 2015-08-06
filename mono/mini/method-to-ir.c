d7b3dacc39dc7bb0b3a5aaab0b6fc33e4725797c
Alexander Kyte <alexmkyte@fastmail.com>
2015-08-06 18:57:08 -0400

    Removed debugging printf

--- mono/mini/method-to-ir.c
+++ mono/mini/method-to-ir.c
@@ -7896,9 +7896,6 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 			MonoExceptionClause *clause = &header->clauses [i];
 			GET_BBLOCK (cfg, try_bb, ip + clause->try_offset);
 
-			MOSTLY_ASYNC_SAFE_PRINTF ("Marking bb at %p(cil_offset_to_bb) [%p - %p] as try_start\n",
-			cfg->cil_offset_to_bb, ip + clause->try_offset, cfg->cil_start);
-
 			try_bb->real_offset = clause->try_offset;
 			try_bb->try_start = TRUE;
 			try_bb->region = ((i + 1) << 8) | clause->flags;
