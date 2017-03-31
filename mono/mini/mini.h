9133ebd38128b0e067376b65d0280a23af72e0c6
Alexander Kyte <alkyte@microsoft.com>
2017-03-31 17:50:59 +0000

    [runtime] Support excluding or choosing to emit only inflated methods

--- mono/mini/mini.h
+++ mono/mini/mini.h
@@ -1897,6 +1897,8 @@ typedef struct {
 	int stat_inlineable_methods;
 	int stat_inlined_methods;
 	int stat_code_reallocs;
+	/* For deduplication */
+	gboolean skip;
 } MonoCompile;
 
 typedef enum {
