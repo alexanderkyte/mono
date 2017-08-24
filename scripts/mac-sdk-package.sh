9626830ecc8da5255ff84d6f126287bcbb465194
Alexander Kyte <alexmkyte@gmail.com>
2017-08-24 16:25:20 -0400

    [runtime] Put libtiff workaround in-tree

--- scripts/mac-sdk-package.sh
+++ scripts/mac-sdk-package.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
+make -C external/bockbuild/builds/tiff-4.0.8-x86 clean || true
+make -C external/bockbuild/builds/tiff-4.0.8-x64 clean || true
+
 external/bockbuild/bb MacSDK --package
